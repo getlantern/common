68a2ead8e07d9f979edf2dc1ef584d02781ddf51
Adam Fisk <afisk@getlantern.org>
2025-03-21 13:02:30 -0600

    Initial pass at new types

--- go.mod
+++ go.mod
@@ -1,5 +1,22 @@
 module github.com/getlantern/common
 
-go 1.18
+go 1.21
 
-require google.golang.org/protobuf v1.28.0
+toolchain go1.23.5
+
+require (
+	github.com/sagernet/sing-box v1.11.5
+	google.golang.org/protobuf v1.36.5
+)
+
+require (
+	github.com/miekg/dns v1.1.63 // indirect
+	github.com/sagernet/sing v0.6.3 // indirect
+	github.com/sagernet/sing-dns v0.4.0 // indirect
+	go4.org/netipx v0.0.0-20231129151722-fdeea329fbba // indirect
+	golang.org/x/mod v0.20.0 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/tools v0.24.0 // indirect
+)
---
+++ types.go
@@ -0,0 +1,28 @@
+package config
+
+import O "github.com/sagernet/sing-box/option"
+
+type UserInfo struct {
+	ProToken string `json:"pro_token,omitempty"`
+	Country  string `json:"country,omitempty"`
+	IP       string `json:"ip,omitempty"`
+}
+
+type ServerLocation struct {
+	Country string `json:"country,omitempty"`
+	City    string `json:"city,omitempty"`
+}
+
+type AvailableServerLocations struct {
+	Servers []ServerLocation `json:"servers,omitempty"`
+}
+
+// Map of outbound tag strings to server locations
+type OutboundLocations map[string]*ServerLocation
+
+type Response struct {
+	UserInfo
+	AvailableServerLocations
+	OutboundLocations
+	O.Options
+}
